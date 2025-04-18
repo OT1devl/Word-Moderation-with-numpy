5a03aa01beb3d96216578277a29050aa8349188f
OT1devl <otidevv1@gmail.com>
2025-04-19 00:31:43 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+DTYPE = np.float32
+
+MAX_LEN = 10
+NUM_VARIANTS = 1000
+model_path = r'\Word-Moderation-with-numpy\models\moderation_network.ot'
+tokenizer_path = r'\Word-Moderation-with-numpy\models\moderation_tokenizer.tok'
+words_path = r'\Word-Moderation-with-numpy\words.json'
